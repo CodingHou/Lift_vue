3c7c7a78089ede586812926a3eb74cdbc8f6e785
侯超 <563009480@qq.com>
2017-09-23 23:55:37 +0800

    修改了spring配置文件，整合了freemarker

---
+++ src/main/java/com/hou/lift/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.hou.lift.controller;
+
+import com.hou.lift.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by houchao on 2016/12/29.
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private IUserService userService;
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index() {
+        System.out.println("=========test=========");
+        return "index";
+    }
+
+}
