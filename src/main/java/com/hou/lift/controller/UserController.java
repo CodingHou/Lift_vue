e92063b2ceb1606320ecbccd121500aaa241644e
侯超 <563009480@qq.com>
2017-09-30 00:15:28 +0800

    修改了需求文档，修改了测试页面

--- src/main/java/com/hou/lift/controller/UserController.java
+++ src/main/java/com/hou/lift/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.hou.lift.controller;
 
+import com.hou.lift.model.User;
 import com.hou.lift.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by houchao on 2016/12/29.
@@ -26,5 +29,15 @@ public class UserController {
         return "userInfo";
     }
 
+
+    @RequestMapping(value = "/users",method = RequestMethod.GET)
+    public String findUserByName(@RequestParam(value = "id",required = false,defaultValue = "")Integer id, ModelMap modelMap){
+        if (null == id) {
+            id = 1;
+        }
+        User user =userService.findUserById(id);
+        modelMap.addAttribute("user2", user);
+        return "/userInfo";
+    }
 
 }
